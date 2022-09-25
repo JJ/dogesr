cfc85a89540bba00d1ed16bfa17c99fb07ad4439
JJ Merelo <jjmerelo@gmail.com>
2022-09-25 20:40:55 +0200

    Eliminating duplicates, closes #6
    And fix non-working tests

--- tests/testthat/test-dogesdata.R
+++ tests/testthat/test-dogesdata.R
@@ -2,6 +2,6 @@ data("doges")
 test_that("data is loaded", {
   expect_vector(data.doges)
   expect_length(data.doges,10)
-  expect(nrow(data.doges),125)
+  expect_equal(nrow(data.doges),129)
 })
 data
