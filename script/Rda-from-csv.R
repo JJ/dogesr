d415285160944b0b7307c1dfd505ef97a6341728
JJ Merelo <jjmerelo@gmail.com>
2022-11-12 19:22:56 +0100

    Putting the century correctly #14

--- script/Rda-from-csv.R
+++ script/Rda-from-csv.R
@@ -2,4 +2,5 @@
 
 data.doges <- read.csv("data-raw/venice-doges.csv")
 data.doges$Years <- data.doges$End - data.doges$Start
+data.doges$Century <- data.doges$Century +1
 save(data.doges, file="data/doges.rda")
