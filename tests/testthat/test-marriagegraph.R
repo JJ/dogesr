9162d30fb4ebd092036acf96493154fb10e758bc
JJ Merelo <jjmerelo@gmail.com>
2023-02-20 08:48:53 +0100

    Adding more meaningful tests

--- tests/testthat/test-marriagegraph.R
+++ tests/testthat/test-marriagegraph.R
@@ -1,5 +1,11 @@
-marriage.graph <- marriage.graph()
+library(igraph)
+this.marriage.graph <- marriage.graph()
 test_that("we have the marriage graph", {
-  expect_true(marriage.graph)
+  expect_type(this.marriage.graph,"list")
 })
+
+test_that("we have specific edges", {
+  expect_true(are_adjacent(this.marriage.graph, "Candiano","Candiano"))
+})
+
 
