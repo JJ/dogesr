bd64c7a046014849c8d15ce342e6e26fd535aeb9
JJ Merelo <jjmerelo@gmail.com>
2023-03-13 12:16:06 +0100

    According to the Wikipedia, https://it.wikipedia.org/wiki/Paolo_Renier

--- script/Rda-from-csv.R
+++ script/Rda-from-csv.R
@@ -4,3 +4,12 @@ data.doges <- read.csv("data-raw/venice-doges.csv")
 data.doges$Years <- data.doges$End - data.doges$Start
 data.doges$Century <- data.doges$Century +1
 save(data.doges, file="data/doges.rda")
+
+doges.marriages.df <- data.frame(data.doges$Family.doge,data.doges$Family.dogaressa)
+doges.marriages.df <- doges.marriages.df[ (doges.marriages.df$data.doges.Family.doge != '' ) & (doges.marriages.df$data.doges.Family.dogaressa != ''),]
+doges.mothers.df <- data.frame(data.doges$Family.doge,data.doges$Family.mother)
+doges.mothers.df <- doges.mothers.df[ (doges.mothers.df$data.doges.Family.doge != '' ) & (doges.mothers.df$data.doges.Family.mother != ''),]
+all.links <- data.frame(doge.or.father = c(doges.marriages.df$data.doges.Family.doge,doges.mothers.df$data.doges.Family.doge),
+                        dogaressa.or.mother = c(doges.marriages.df$data.doges.Family.dogaressa,doges.mothers.df$data.doges.Family.mother))
+doges.marriages.sn <- graph.data.frame(all.links,directed=F)
+save(doges.marriages.sn, file="data/doges.marriages.rda")
