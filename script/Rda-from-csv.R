5aef1c8ffb6cf0f2073162c5556410f7ff0c2f90
JJ Merelo <jjmerelo@gmail.com>
2023-05-24 18:10:58 +0200

    :sparkles: adding a new file to data #27
    Generating code

--- script/Rda-from-csv.R
+++ script/Rda-from-csv.R
@@ -1,10 +1,14 @@
 #!/usr/bin/env Rscript
 
 library(igraph)
+library(dplyr)
 data.doges <- read.csv("data-raw/venice-doges.csv")do
 data.doges$Years <- data.doges$End - data.doges$Start
 data.doges$Century <- data.doges$Century +1
 save(data.doges, file="data/doges.rda")
+
+doge.families <- data.doges %>% count(Family.doge,sort=T)
+save(doge.families, file="data/doge.families.rda")
 
 doges.marriages.df <- data.frame(data.doges$Family.doge,data.doges$Family.dogaressa)
 doges.marriages.df <- doges.marriages.df[ (doges.marriages.df$data.doges.Family.doge != '' ) & (doges.marriages.df$data.doges.Family.dogaressa != ''),]
