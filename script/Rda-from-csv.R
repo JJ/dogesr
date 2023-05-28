7eee5a03594b5e85f74e465efdbb9a6dd069d093
JJ Merelo <jjmerelo@gmail.com>
2023-05-28 12:17:39 +0200

    Fixed data for family names, closes #28
    Closes #27 since that was a blocker :bug:

--- script/Rda-from-csv.R
+++ script/Rda-from-csv.R
@@ -2,7 +2,7 @@
 
 library(igraph)
 library(dplyr)
-data.doges <- read.csv("data-raw/venice-doges.csv")do
+data.doges <- read.csv("data-raw/venice-doges.csv")
 data.doges$Years <- data.doges$End - data.doges$Start
 data.doges$Century <- data.doges$Century +1
 save(data.doges, file="data/doges.rda")
