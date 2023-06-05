3967e744a3b330d9de700a3e1336b85915011bf3
JJ Merelo <jjmerelo@gmail.com>
2023-06-05 09:04:01 +0200

    Fixes #18 with a test :white_check_mark:

--- tests/testthat/test-dogesyearsdata.R
+++ tests/testthat/test-dogesyearsdata.R
@@ -2,7 +2,7 @@ data.doges.years <- doges.years()
 test_that("data doges years is loaded", {
   expect_vector(data.doges.years)
   expect_length(data.doges.years,6)
-  expect_equal(nrow(data.doges.years),122)
+  expect_equal(nrow(data.doges.years),123)
 })
 
 test_that("There are no gaps in doges", {
