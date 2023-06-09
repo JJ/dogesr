666a44730daa8565061901076d9afa4c12cc784e
JJ Merelo <jjmerelo@gmail.com>
2023-06-09 19:30:19 +0200

    Get rid of more warnings :white_check_mark: #31

--- R/marriage.graph.R
+++ R/marriage.graph.R
@@ -4,7 +4,7 @@ globalVariables(c("doges.marriages.sn","family.types"))
 
 #' Convert doges data into a social graph
 #'
-#' @import igraph
+#' @importFrom igraph V V<-
 #'
 #' @return A graph with noble families as nodes, edges representing marriages of doges or parents; the type of family is the attribute family.type
 #' @export
