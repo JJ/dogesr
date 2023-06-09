bed424a6a37b76cc016c01dc2171d5da7a0359f0
JJ Merelo <jjmerelo@gmail.com>
2023-06-09 11:05:14 +0200

    Changing tests also, closes #30

--- tests/testthat/test-dogesyearsdata.R
+++ tests/testthat/test-dogesyearsdata.R
@@ -1,12 +1,11 @@
-data.doges.years <- doges.years()
+data("doges.years")
 test_that("data doges years is loaded", {
-  expect_vector(data.doges.years)
-  expect_length(data.doges.years,6)
-  expect_equal(nrow(data.doges.years),123)
+  expect_vector(doges.years)
+  expect_length(doges.years,6)
+  expect_equal(nrow(doges.years),123)
 })
 
 test_that("There are no gaps in doges", {
-  doges.years <- unique(data.frame(data.doges[,c("Doge","Start","End")]))
   for ( doge in 1:90){
     expect_equal(doges.years[doge,]$End, doges.years[doge+1,]$Start)
   }
